aa9b6283465135b54cfa659f9544a94e7d9528be
xyl <416319808@qq.com>
2023-07-06 23:57:22 +0800

    配置方案

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	config2 "bubble/config"
+	"bubble/pkg/config"
 	"bubble/routes"
+	"flag"
 	"github.com/xylong/bingo"
 )
 
+func init() {
+	config2.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	bingo.Init().
 		Mount("v1", routes.Controllers...)().
-		Lunch(8080)
+		Lunch(config.GetInt("app.port"))
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"bubble/pkg/helpers"
+	"github.com/spf13/cast"
 	viperlib "github.com/spf13/viper"
 	"os"
 )
@@ -60,4 +62,72 @@ func loadConfig() {
 	for name, f := range ConfigFuncs {
 		viper.Set(name, f())
 	}
+}
+
+// Env 读取环境变量，支持默认值
+func Env(name string, defaultValue ...interface{}) interface{} {
+	if len(defaultValue) > 0 {
+		return internalGet(name, defaultValue[0])
+	}
+
+	return internalGet(name)
+}
+
+func internalGet(path string, defaultValue ...interface{}) interface{} {
+	// config 或者环境变量不存在的情况
+	if !viper.IsSet(path) || helpers.Empty(viper.Get(path)) {
+		if len(defaultValue) > 0 {
+			return defaultValue[0]
+		}
+
+		return nil
+	}
+
+	return viper.Get(path)
+}
+
+// Add 新增配置项
+func Add(name string, configFn ConfigFunc) {
+	ConfigFuncs[name] = configFn
+}
+
+// Get 获取配置项
+// 第一个参数 path 允许使用点式获取，如：app.name
+// 第二个参数允许传参默认值
+func Get(path string, defaultValue ...interface{}) string {
+	return GetString(path, defaultValue...)
+}
+
+func GetString(path string, defaultValue ...interface{}) string {
+	return cast.ToString(internalGet(path, defaultValue))
+}
+
+// GetInt 获取 Int 类型的配置信息
+func GetInt(path string, defaultValue ...interface{}) int {
+	return cast.ToInt(internalGet(path, defaultValue...))
+}
+
+// GetFloat64 获取 float64 类型的配置信息
+func GetFloat64(path string, defaultValue ...interface{}) float64 {
+	return cast.ToFloat64(internalGet(path, defaultValue...))
+}
+
+// GetInt64 获取 Int64 类型的配置信息
+func GetInt64(path string, defaultValue ...interface{}) int64 {
+	return cast.ToInt64(internalGet(path, defaultValue...))
+}
+
+// GetUint 获取 Uint 类型的配置信息
+func GetUint(path string, defaultValue ...interface{}) uint {
+	return cast.ToUint(internalGet(path, defaultValue...))
+}
+
+// GetBool 获取 Bool 类型的配置信息
+func GetBool(path string, defaultValue ...interface{}) bool {
+	return cast.ToBool(internalGet(path, defaultValue...))
+}
+
+// GetStringMapString 获取结构数据
+func GetStringMapString(path string) map[string]string {
+	return viper.GetStringMapString(path)
 }
